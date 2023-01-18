ac52026aa2d1faf65b392c3ba4dfcaf0bfb6f5d3
juli <julitati25@gmail.com>
2023-01-18 17:54:35 +0400

    add folder for constants

---
+++ src/constants/info.js
@@ -0,0 +1,85 @@
+export const skills= {
+  title: "Skills",
+  subtitle: "MY TOP SKILLS",
+  skills_content: [
+    {
+      name: "HTML",
+      logo: "../images/figma.png"
+    },
+    {
+      name: "CSS",
+      logo: "../images/css.png"
+    },
+    {
+      name: "Javascript",
+      logo: "../images/javascript.png"
+    },
+    {
+      name: "React js",
+      logo: "../images/react.jpg"
+    },
+    {
+      name: "Next js",
+      logo: "../images/nextjs.png"
+    },
+    {
+      name: "Figma",
+      logo: "../images/figma.png"
+    }
+  ]
+};
+
+export const ProjectContent = [
+  {
+    title: "Online grocery store",
+    img: "../images/grocery-store.png",
+    txt: "Check my website simulator of an online grocery store. Add, delete products once your ready checkout!",
+    link: "https://onlinegrocerystore.vercel.app/"
+  },
+  {
+    title: "Company analytics website",
+    img: "../images/analytics-website.png",
+    txt: "This app uses recharts.js to display charts with mocked data about company sales, number of items sold etc. You can also apply filters to view the data by week or by month.",
+    link: "https://yourcompanyanalytics.vercel.app/"
+
+  },
+  {
+    title: "Personality test",
+    img: "../images/personality-website",
+    txt: "Answer four simple questions and get to know if you are more of an introvert or an extrovert",
+    link: "https://personality-test-theta.vercel.app/"
+  },
+  {
+    title: "Shopping cart app with Typescript",
+    img: "../images/shopping-cart.png",
+    txt: "Simple app that calculates the price of items added to the shopping bag. Add, remove, increase or decrease the quantities of the item. Uses local storage.",
+    link: "https://shopping-cart-typescript-smoky.vercel.app/"
+  },
+  {
+    title: "Recipe finder",
+    img: "../images/recipe-finder-website.png",
+    txt: "Search your favorite recipe, filter them by diet or set only for gluten free ones!. Once you like on click on details to check their nutrional value, ingredients.",
+    link: "https://recipes-finder-a0xpb9izs-julita25.vercel.app/"
+  },
+  {
+    title: "Cold Stone landing page",
+    img: "../images/cold-stone-website.png",
+    txt: "Check this simple website where I create a landing page for my favorite ice cream chain!.",
+    link: "https://hopeful-babbage-d198ef.netlify.app"
+  }
+];
+
+export const TestimonialsContent = [
+  {
+    review: "Juliette's quick intellect and thirst for self-improvement make her an outstanding employee. I always could rely on her to meet the deadlines and exceed our expectations",
+    name: "Nicolo Manica, CEO uDroppy"
+  },
+  {
+    review: "Working with Juliette has been an extraordinary experience. Her commitment to the team and the project has always been extraordinary. She never backs off when there is need to go for the extra mile to get tasks done",
+    name: "Asdullah Siddique, CTO uDroppy"
+  },
+  {
+    review: "uliette really shines in her passion about engineering and willingness to learn. She is a quick-learner and is always motivated to be a better and better engineer!",
+    name: "Dinny Peter, CTO Starlings Solutions"
+  }
+];
---
+++ src/constants/keys.js
@@ -0,0 +1,3 @@
+export const SERVICE_ID = "contact_service";
+export const TEMPLATE_ID = "contact_template";
+export const ACCESS_KEY = "Sfs-BBdIuRfufK7Kh";
