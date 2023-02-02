bcd468abdff7ad82bfdf07554b616bf9a502ded2
juli <julitati25@gmail.com>
2023-02-02 17:00:27 +0400

    add link to v2x project

--- src/components/Awards.jsx
+++ src/components/Awards.jsx
@@ -16,17 +16,18 @@ const Awards = () => {
         <div className="flex flex-col lg:flex-row">
           <div className="p-16 w-full md:p-10 lg:w-2/4">
             <div
+              data-aos="fade-left"
               className="duration-500 bg-bg_light_primary mx-8 border-2 p-8 h-full rounded-2xl flex items-center flex-col border-slate-200"
             >
               <div className="flex flex-col gap-5">
-                <img src={award2} alt="DAST AWARDr" className="object-cover w-full" />
+                <img src={award2} alt="DAST AWARDr" className="object-cover w-full" data-aos="fade-right" />
                 <p className="sm:text-base text-sm">
                   On May 11th 2022, I was awarded with the <span className="font-bold text-blue-600">Dubai Award For Sustainable Transport (DAST)</span>. This award, given by the Road Transport Authority (RTA) of Dubai
                   is given each year to transport systems initiatives in the field of safety, innovation and environmental protection. My project was <span className="font-bold text-blue-600">Finding a suitable V2X protocol for Dubai</span>,
                   and evaluated though simulations the way in which we could network protocols that enable communication amongst cars and their surroundings to prevent
                   road accidents in Dubai.
                 </p>
-                <a href="" target="_blank" rel="noreferrer">
+                <a href="https://final-project-website.vercel.app/" target="_blank" rel="noreferrer">
                   <button className="text-white border-2 p-2 bg-blue-400">
                     Find more
                   </button>
